f15b0bb93facf7a49d5e63699f2940438efbad75
VoroninaNataliia <voronina.nataliya0702@gmail.com>
2023-01-24 09:45:08 +0100

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+btnStartEl.addEventListener('click', onStartClick);
+btnStopEl.addEventListener('click', onStopClick);
+
+function switchColors() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+let timer;
+
+function onStartClick() {
+  timer = setInterval(switchColors, 1000);
+  btnStartEl.toggleAttribute('disabled');
+}
+
+function onStopClick() {
+  clearInterval(timer);
+  btnStartEl.removeAttribute('disabled');
+}
